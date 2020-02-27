c52efc716ec9bf2a4f3d7daf91ac7a275480af25
Shantanoo Desai <des@biba.uni-bremen.de>
2020-02-27 13:21:42 +0100

    v1.4 Update with MQTT publishing
    - Improve Argument handling for the script
    - Add MQTT Publishing capabilites
    - Add Linting

--- gpsinflux/gpsinflux.py
+++ gpsinflux/gpsinflux.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 # Python3 Command Line Utility to read from MTK GPS Module (Adafruit) connected
-# to serial ports and store GPS relevant information to InfluxDB either via UDP
-# or HTTP
+# to serial ports and store GPS relevant information to InfluxDB via UDP and
+# publishing the data to MQTT topics
 # Author: Shantanoo Desai <des@biba.uni-bremen.de>
+# Changes:
+# - Change `measurement` dict
+# - Create Line Protocol Strings from `measurement` dict
+# - Add Multiple Publishing to respective MQTT Topics
 
+import os
 import sys
-import struct
 import logging
 import json
 import time
-import datetime
 import argparse
+import concurrent.futures
 import serial
 import pynmea2
 from .mt3339 import mt3339
-from influxdb import InfluxDBClient
+from influxdb import InfluxDBClient, line_protocol
 from influxdb.client import InfluxDBClientError
+import paho.mqtt.publish as publish
 
 # Logger Settings
-logger = logging.getLogger("main")
-logger.setLevel(logging.ERROR)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 handler = logging.FileHandler("/var/log/gpsinflux.log")
 handler.setLevel(logging.ERROR)
@@ -28,164 +33,217 @@ formatter = logging.Formatter('%(asctime)s-%(name)s-%(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-CONF_PATH = '/etc/umg/conf.json'
-
+DEVICE = ''
 com = None
-
 client = None
+mqtt_conf = dict()
+
 
 def gps_setup(serialport, baudrate, updaterate):
+    """ Setup GPS Device Baud and Update Rates and
+        read only RMC Sentences"""
     try:
         gps = mt3339(serialport)
         gps.set_baudrate(baudrate)
         gps.set_nmea_update_rate(updaterate)
-        gps.set_nmea_output(gll = 0, rmc = 3, vtg = 0, gga = 0, gsa = 0, gsv = 0)
+        gps.set_nmea_output(gll=0, rmc=3, vtg=0, gga=0, gsa=0, gsv=0)
     except Exception as e:
-        logger.exception('Exception Occured while setting up GPS')
+        print('Exception Occured while setting up GPS')
         raise(e)
 
 
-def send_data(serialport, baudrate, updaterate, db_host, db_port, udp_port):
-    logger.info('Setting Up GPS')
-    logger.debug('GPS Config: %s %d %d' % (serialport, baudrate, updaterate))
-    gps_setup(serialport, baudrate, updaterate)
-
-    global client
-    logger.info('UDP Sending selected')
+def publish_data(lineprotocol_data):
+    """ Publish Data points as line protocol strings to MQTT Broker
+    """
+    lp_array = lineprotocol_data.split('\n')
+    lp_array.pop()  # remove the last newline from the array
+    publish_messages = []
+    global mqtt_conf
+    for topic in mqtt_conf['topics']:
+        mqtt_msg = {
+            'topic': DEVICE + '/' + topic,
+            'payload': lp_array[mqtt_conf['topics'].index(topic)],
+            'qos': 1,
+            'retain': False
+        }
+        publish_messages.append(mqtt_msg)
+    logger.debug(publish_messages)
     try:
-        client = InfluxDBClient(host=db_host, port=db_port, use_udp=True, udp_port=udp_port)
-        logger.info('InfluxDB Client Created for UDP Sending')
-    except InfluxDBClientError as e:
-        logger.exception('Exception while InfluxDB Client Creation for UDP')
-        raise(e)
+        publish.multiple(
+            publish_messages,
+            hostname=mqtt_conf['broker'],
+            port=mqtt_conf['port'])
+        return True
+    except Exception as e:
+        logger.error(e)
+        return False
 
+
+def save_to_db(measurements):
+    """Save Data to InfluxDB
+    """
+    global client
+    try:
+        client.send_packet(measurements)
+        return True
+    except InfluxDBClientError as influx_e:
+        logger.error('Error while UDP sending: {e}'.format(e=influx_e))
+        return False
+
+
+def read_from_gps(serialport, baudrate, updaterate):
+    logger.info('Setting Up GPS')
+    # NOTE: Comment the line below out when using Adafruit GPS Wing
+    # gps_setup(serialport, baudrate, updaterate)
+    global DEVICE
     # dict layout for UDP Packet
     measurement = {
         "tags": {
-            "type": "RMC",
+            "source": "gps"
         },
         "points": [
             {
-                "measurement": "gps",
+                "measurement": "location",
                 "fields": {
-                    "lat": 0.0,
-                    "lon": 0.0,
-                    "sog": 0.0,
-                    "cog": 0.0,
-                    "status": 0.0
+                        "lat": 0.0
+                }
+            },
+            {
+                "measurement": "location",
+                "fields": {
+                    "lon": 0.0
+                }
+            },
+            {
+                "measurement": "groundVelocity",
+                "fields": {
+                        "sog": 0.0
+                }
+            },
+            {
+                "measurement": "groundVelocity",
+                "fields": {
+                        "cog": 0.0
                 }
             }
         ]
     }
-    logger.debug(measurement)
+
+    # Create Serial Connection and NMEA Stream Reader /Parser
     global com
     reader = pynmea2.NMEAStreamReader(errors='ignore')
-    try:
-        com = serial.Serial(serialport, baudrate, timeout=5.0)
-        logger.info('Created Connection to Serial Port')
-
-    except serial.SerialException as e:
-        logger.exception('Exception Occured while Creation of SerialPort Connection')
-        raise(e)
+    com = serial.Serial(serialport, baudrate, timeout=5.0)
+    logger.info('Created Connection to Serial Port')
 
     while True:
         try:
             data = com.read(16).decode('utf-8')
+            for msg in reader.next(data):
+                dat = pynmea2.parse(str(msg).strip('\r\n'), check=True)
+                logger.debug('NMEA Sentence: {d}'.format(d=dat))
+                try:
+                    if not (dat.latitude == 0.0 and dat.longitude == 0.0):
+                        if isinstance(dat, pynmea2.RMC):
+                            timestamp = int(time.time())
+                            measurement["points"][0]["fields"]["lat"] = dat.latitude
+                            measurement["points"][1]["fields"]["lon"] = dat.longitude
+                            measurement["points"][2]["fields"]["sog"] = dat.spd_over_grnd
+                            measurement["points"][3]["fields"]["cog"] = dat.true_course
+                            for point in measurement['points']:
+                                # insert timestamp to each point
+                                point['time'] = timestamp
+                            with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+                                if executor.submit(publish_data, line_protocol.make_lines(measurement)).result():
+                                    logger.info('MQTT Publish Success')
+                                if executor.submit(save_to_db, measurement).result():
+                                    logger.info('Data saved to InfluxDB')
+                    else:
+                        logger.info("GPS Location not yet available.")
+                except Exception as e:
+                    logger.error(e)
         except Exception as e:
-            logger.exception('Exception while Reading from Serial Port')
-            pass
+            logger.error('Exception Serial Port Read: {e}'.format(e=e))
+            com.close()
+            client.close()
+            sys.exit(2)
 
-        for msg in reader.next(data):
-            dat = pynmea2.parse(str(msg).strip('\r\n'), check=True)
-            print(dat)
-            try:
-                if not (dat.latitude == 0.0 and dat.longitude == 0.0):
-                    if isinstance(dat, pynmea2.RMC):
-                        
-                        measurement["points"][0]["fields"]["lat"] = dat.latitude
-                        measurement["points"][0]["fields"]["lon"] = dat.longitude
-                        measurement["points"][0]["fields"]["sog"] = dat.spd_over_grnd
-                        measurement["points"][0]["fields"]["cog"] = dat.true_course
-                        measurement["time"] = time.time()
-                        try:
-                            client.send_packet(measurement)
-                        except InfluxDBClientError as e:
-                            logger.exception('Exception Occured while Sending UDP Packet')
-                            raise(e)
-                else:
-                    print("GPS Location not yet available.")
-            except Exception as e:
-                pass
+
+def file_path(path_to_file):
+    """Check if Path and File exist for Configuration"""
+
+    if os.path.exists(path_to_file):
+        if os.path.isfile(path_to_file):
+            logger.info('File Exists')
+            with open(path_to_file) as conf_file:
+                return json.load(conf_file)
+        else:
+            logger.error('Configuration File Not Found')
+            raise FileNotFoundError(path_to_file)
+    else:
+        logger.error('Path to Configuration File Not Found')
+        raise NotADirectoryError(path_to_file)
+
 
 def parse_args():
-    """Pass Arguments if passed else use configuration file"""
+    """Pass Arguments for configuration file"""
 
-    parser = argparse.ArgumentParser(description='CLI for acquiring GPS values and storing it in InfluxDB')
-
-    parser.add_argument('--serialport', type=str, required=False, help='Serial Port for GPS Module')
-
-    parser.add_argument('--baudrate', type=int, required=False, default=9600, help='Baud Rate for GPS Module Default: 9600')
-
-    parser.add_argument('--updaterate', type=int, required=False, default=1000, help='Update Rate for GPS Module in ms. Default: 1000ms')
-
-    parser.add_argument('--db-host', type=str, required=False, default='localhost',
-                        help='hostname for InfluxDB. Default: `localhost`')
-
-    parser.add_argument('--db-port', type=int, required=False, default=8086,
-                        help='port number for InfluxDB. Default: 8086')
-
-    parser.add_argument('--udp-port', type=int, required=False,
-                        help='UDP Port for sending information via UDP. Should also be configured in `influxdb.conf`')
+    parser = argparse.ArgumentParser(description='CLI for acquiring GPS values, storing it in InfluxDB and publishing via MQTT')
+    parser.add_argument('--config', type=str, required=True, help='configuration conf.json file with path.')
 
     return parser.parse_args()
 
+
 def main():
+    """Initial Setup of Clients based on Configuration File"""
     args = parse_args()
     # print(args)
-    CONF = dict()
+    CONF = file_path(args.config)
+    gps_conf = CONF['gps']
+    influx_conf = CONF['influx']
 
-    if len(sys.argv) == 1:
-        logger.debug('Starting Script in Default Mode. Reading Conf File in %s' %CONF_PATH)
+    global mqtt_conf
+    mqtt_conf = CONF['mqtt']
 
-        with open(CONF_PATH) as cFile:
-            _conf = json.load(cFile)
-            CONF = _conf['gps'] # store conf for GPS
-            logger.debug('CONF: ' + json.dumps(CONF))
+    global DEVICE
+    DEVICE = CONF['deviceID']
 
-        try:
-            send_data(serialport = CONF['serialport'],
-                      baudrate=CONF['baudrate'],
-                      updaterate=CONF['updaterate'],
-                      db_host=args.db_host,
-                      db_port=args.db_port,
-                      udp_port=CONF['dbConf']['udp_port']
-                      )
-        except KeyboardInterrupt:
-            logger.exception('CTRL+C hit for Default Configuration')
-            com.close()
-            client.close()
-            sys.exit(0)
+    logger.info('Creating InfluxDB Client')
+    logger.debug('Client for {influx_host}@{influx_port} with udp:{udp_port}'.format(
+        influx_host=influx_conf['host'],
+        influx_port=influx_conf['port'],
+        udp_port=influx_conf['udp_port']))
+    global client
+    client = InfluxDBClient(
+        host=influx_conf['host'],
+        port=influx_conf['port'],
+        use_udp=True,
+        udp_port=influx_conf['udp_port'])
+    logger.info('Checking Connectivity to InfluxDB')
+    try:
+        if client.ping():
+            logger.info('Connected to InfluxDB')
+        else:
+            logger.error('Cannot Connect to InfluxDB. Check Configuration/Connectivity')
+    except Exception as connection_e:
+        logger.error(connection_e)
+        client.close()
+        sys.exit(1)
 
-    elif len(sys.argv) > 1:
-        if args.serialport is None:
-            print('Please Mention SerialPort for GPS Module')
-            sys.exit(1)
-        if args.udp_port == None:
-            print('Please Mention UDP Port for Sending Values')
-            sys.exit(1)
-        print('Starting Script with Custom Arguments')
-        try:
-            send_data(serialport = args.serialport,
-                        baudrate=args.baudrate,
-                        updaterate=args.updaterate,
-                        db_host=args.db_host,
-                        db_port=args.db_port,
-                        udp_port=args.udp_port)
-        except KeyboardInterrupt:
-            logger.exception('CTRL+C hit for Default Configuration')
-            com.close()
-            client.close()
-            sys.exit(0)
+    logger.info('Connecting to GPS Device')
+    logger.debug('Device @ {port}, with baud={baud} & update={upd}'.format(
+        port=gps_conf['serialport'],
+        baud=gps_conf['baudrate'],
+        upd=gps_conf['updaterate']))
+    try:
+        read_from_gps(
+            serialport=gps_conf['serialport'],
+            baudrate=gps_conf['baudrate'],
+            updaterate=gps_conf['updaterate'])
+    except KeyboardInterrupt:
+        print('CTRL+C hit for Default Configuration')
+        com.close()
+        client.close()
+        sys.exit(0)
 
 
 if __name__ == '__main__':
